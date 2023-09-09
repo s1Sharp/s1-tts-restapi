e32f44cca873ef60b530ae4686b3c3e37240e2cf
Tsarkov Maksim <mtsarkov@naumen.ru>
2023-09-10 02:15:19 +0300

    gracefull shutdown

--- cmd/s1-tts-restapi/server.go
+++ cmd/s1-tts-restapi/server.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/s1Sharp/s1-tts-restapi/controller"
 	"github.com/s1Sharp/s1-tts-restapi/internal/config"
@@ -75,5 +79,38 @@ func (s ttsServer) Run() error {
 		}
 	}()
 
-	return s.server.Run(":" + s.cfg.HttpAddr)
+	srv := &http.Server{
+		Addr:    ":" + s.cfg.HttpAddr,
+		Handler: s.server,
+	}
+
+	// Graceful server shutdown - https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/server.go
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to initialize server: %v\n", err)
+		}
+	}()
+
+	log.Printf("Listening on port %v\n", srv.Addr)
+
+	// Wait for kill signal of channel
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// This blocks until a signal is passed into the quit channel
+	<-quit
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Shutdown server
+	log.Println("Shutting down server...")
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v\n", err)
+		return err
+	}
+	return nil
 }
